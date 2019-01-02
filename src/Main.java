9fb94246345dbdbd9b0e1283bce55545a8886b12
Iscle <albertiscle9@gmail.com>
2019-01-02 19:23:56 +0100

    Funciona

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 import com.google.gson.Gson;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Comparator;
@@ -11,99 +10,181 @@ public class Main {
 
     private static Node[] nodes;
     private static Server[] servers;
+    private static ServerPlus[] serversPlus;
     private static User[] users;
 
     public static void main(String[] args) {
         System.out.println("Reading JSON...");
-        readJSON();
+
+        if (!readJSON()) {
+            System.out.println("No s'han pogut llegir tots els fitxers!");
+            return;
+        }
+
         System.out.println("\tNodes: " + nodes.length);
         System.out.println("\tServers: " + servers.length);
         System.out.println("\tUsers: " + users.length);
+
+        //System.out.println(branchAndBoundFiabilitat(1, 10).getBound());
+        System.out.println(branchAndBoundSalts(1, 10).getBound());
     }
 
-    private static boolean readJSON() {
-        try {
-            BufferedReader br = new BufferedReader(new FileReader("nodes.json"));
-            nodes = new Gson().fromJson(br, Node[].class);
-            br = new BufferedReader(new FileReader("servers.json"));
-            servers = new Gson().fromJson(br, Server[].class);
-            br = new BufferedReader(new FileReader("users.json"));
-            users = new Gson().fromJson(br, User[].class);
-            return true;
-        } catch (FileNotFoundException e) {
-            return false;
-        }
-    }
-
-    private boolean isSolutionSalts(Solution s) {
-        return false;
-    }
-
-    private boolean isPromisingSalts(Solution s) {
-        return false;
-    }
-
-    private boolean isSolutionFiabilitat(Solution s) {
-        return false;
-    }
-
-    private boolean isPromisingFiabilitat(Solution s) {
-        return false;
-    }
-
-    private Solution[] branchAndBound() {
-        // TODO: change initial nodes from solutions
-        Solution xSalts = new Solution(nodes[0]);
-        Solution xFiabilitat = new Solution(nodes[0]);
-        Solution bestSalts = new Solution(nodes[0]);
-        Solution bestFiabilitat = new Solution(nodes[0]);
-        PriorityQueue<Solution> liveNodesSalts = new PriorityQueue<>(11, new Comparator<Solution>() {
+    private static Solution branchAndBoundSalts(int start, int end) {
+        Solution x;
+        Solution best = null;
+        PriorityQueue<Solution> liveNodes = new PriorityQueue<>(11, new Comparator<Solution>() {
             @Override
             public int compare(Solution o1, Solution o2) {
-                return 0;
+                return Double.compare(o1.getBound(), o2.getBound());
             }
         });
-        PriorityQueue<Solution> liveNodesFiabilitat = new PriorityQueue<>(11, new Comparator<Solution>() {
-            @Override
-            public int compare(Solution o1, Solution o2) {
-                return 0;
+        ArrayList<Solution> options;
+
+        x = new Solution(getNodeById(start), false);
+        liveNodes.add(x);
+
+        while (liveNodes.size() > 0) {
+            x = liveNodes.poll();
+            options = expand(x);
+
+            for (Solution s:options) {
+                System.out.println("\nNew solution: ");
+                System.out.println("\tCost: " + s.getBound());
+                for (Node n:s.getPath()) {
+                    System.out.println("\t" + n.getId());
+                }
             }
-        });
-        ArrayList<Solution> options = new ArrayList<>();
 
-        liveNodesSalts.add(xSalts);
-        liveNodesFiabilitat.add(xFiabilitat);
-
-        while (liveNodesSalts.size() != 0 && liveNodesFiabilitat.size() != 0) {
-            xSalts = liveNodesSalts.poll();
-            xFiabilitat = liveNodesFiabilitat.poll();
-
-            for (Solution option : options) {
-                if (isSolutionSalts(option)) {
-                    bestSalts = min(option, bestSalts);
-                } else if (isPromisingSalts(option)) {
-                    liveNodesSalts.add(option);
-                } else if (isSolutionFiabilitat(option)) {
-                    bestFiabilitat = max(option, bestFiabilitat);
-                } else if (isPromisingFiabilitat(option)) {
-                    liveNodesFiabilitat.add(option);
+            for (Solution option:options) {
+                if (isSolution(end, option)) {
+                    best = min(option, best);
+                } else if (isPromising(option, best)) {
+                    liveNodes.add(option);
                 }
             }
         }
 
-        return new Solution[] {bestSalts, bestFiabilitat};
+        return best;
     }
 
-    public Solution max(Solution o1, Solution o2) {
-        if (o1.getCost() > o2.getCost()) {
+    private static Solution branchAndBoundFiabilitat(int start, int end) {
+        Solution x;
+        Solution best = null;
+        PriorityQueue<Solution> liveNodes = new PriorityQueue<>(11, new Comparator<Solution>() {
+            @Override
+            public int compare(Solution o1, Solution o2) {
+                return Double.compare(o2.getBound(), o1.getBound());
+            }
+        });
+        ArrayList<Solution> options;
+
+        x = new Solution(getNodeById(start), true);
+        liveNodes.add(x);
+
+        while (liveNodes.size() > 0) {
+            x = liveNodes.poll();
+            options = expand(x);
+
+            for (Solution option:options) {
+                if (isSolution(end, option)) {
+                    best = max(option, best);
+                } else if (isPromising(option, best)) {
+                    liveNodes.add(option);
+                }
+            }
+        }
+
+        return best;
+    }
+
+    private static boolean readJSON() {
+        try {
+            //nodes = new Gson().fromJson(new BufferedReader(new FileReader("nodes.json")), Node[].class);
+            nodes = new Gson().fromJson(new BufferedReader(new FileReader("nodes_plus.json")), Node[].class);
+            servers = new Gson().fromJson(new BufferedReader(new FileReader("servers.json")), Server[].class);
+            serversPlus = new Gson().fromJson(new BufferedReader(new FileReader("servers_plus.json")), ServerPlus[].class);
+            users = new Gson().fromJson(new BufferedReader(new FileReader("users.json")), User[].class);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public static Node getNodeById(int id) {
+        for (Node n:nodes) {
+            if (n.getId() == id) {
+                return n;
+            }
+        }
+
+        return null;
+    }
+
+    private static ArrayList<Solution> expand(Solution x) {
+        ArrayList<Solution> solutions = new ArrayList<>();
+        Solution s;
+
+        for (NodeConnection nc:x.getLast().getConnectsTo()) {
+            Node node = getNodeById(nc.getTo());
+            if (!x.isVisited(node)) {
+                s = new Solution(x);
+                if (s.isReliability()) {
+                    s.addNode(node);
+                } else {
+                    s.addNode(node, nc.getCost());
+                }
+                solutions.add(s);
+            }
+        }
+
+        return solutions;
+    }
+
+    private static boolean isSolution(int end, Solution option) {
+        return option.getLast().getId() == end;
+    }
+
+    private static boolean isPromising(Solution option, Solution best) {
+        if (best == null) {
+            return true;
+        }
+
+        if (option.isReliability()) {
+            return option.getBound() > best.getBound();
+        } else {
+            return option.getBound() < best.getBound();
+        }
+
+
+    }
+
+    private static Solution max(Solution o1, Solution o2) {
+        if (o1 == null) {
+            return o2;
+        }
+
+        if (o2 == null) {
+            return o1;
+        }
+
+        if (o1.getBound() > o2.getBound()) {
             return o1;
         } else {
             return o2;
         }
     }
 
-    public Solution min(Solution o1, Solution o2) {
-        if (o1.getCost() > o2.getCost()) {
+    private static Solution min(Solution o1, Solution o2) {
+        if (o1 == null) {
+            return o2;
+        }
+
+        if (o2 == null) {
+            return o1;
+        }
+
+        if (o1.getBound() > o2.getBound()) {
             return o2;
         } else {
             return o1;
--- src/Solution.java
+++ src/Solution.java
@@ -1,25 +1,65 @@
+import java.util.ArrayList;
+
 public class Solution {
-    private double cost;
-    private Node rootNode;
+    private ArrayList<Node> path;
+    private int last;
+    private double bound;
+    private boolean isReliability;
 
-    public Solution(Node rootNode) {
-        this.cost = -1;
-        this.rootNode = rootNode;
+    public Solution(Node node, boolean isReliability) {
+        this.path = new ArrayList<>();
+        this.path.add(node);
+        this.last = 0;
+        if (isReliability) {
+            this.bound = node.getReliability();
+        } else {
+            this.bound = 0;
+        }
+        this.isReliability = isReliability;
     }
 
-    public double getCost() {
-        return cost;
+    public Solution(Solution s) {
+        this.path = (ArrayList) s.path.clone();
+        this.last = s.last;
+        this.bound = s.bound;
+        this.isReliability = s.isReliability;
     }
 
-    public void setCost(double cost) {
-        this.cost = cost;
+    public ArrayList<Node> getPath() {
+        return path;
     }
 
-    public Node getRootNode() {
-        return rootNode;
+    public double getBound() {
+        return bound;
     }
 
-    public void setRootNode(Node rootNode) {
-        this.rootNode = rootNode;
+    public void addNode(Node node) {
+        path.add(node);
+        bound *= node.getReliability();
+        ++last;
+    }
+
+    public void addNode(Node node, double cost) {
+        path.add(node);
+        bound += cost;
+        ++last;
+    }
+
+    public Node getLast() {
+        return path.get(last);
+    }
+
+    public boolean isReliability() {
+        return isReliability;
+    }
+
+    public boolean isVisited(Node n) {
+        for (Node node:path) {
+            if (n == node) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
